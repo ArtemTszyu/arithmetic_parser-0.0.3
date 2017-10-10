857656b02451c65f4c2f72a3057fbde457bbacc7
Alexandr Pozhuev <32191131+SashaPozhuev1@users.noreply.github.com>
2017-10-10 23:30:05 +0300

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,81 @@
+#include <iostream>
+#include <sstream>
+#include <cstring>
+
+using namespace std;
+
+int main()
+{	
+	float result, peremA, peremP;
+	char znak, znakDo = '*';
+	
+	string stroka;
+	getline(cin, stroka);
+	stroka = "0" + stroka;
+
+	istringstream stream(stroka);
+
+	stream >> result;
+	
+	while (stream >> znak) {
+    stream >> peremA;
+
+		if (znak == '+') {
+			result += peremA;
+			znakDo = '+';
+			peremP = peremA;
+		}
+		
+    else if (znak == '-') {
+			result -= peremA;
+			znakDo = '-';
+			peremP = peremA;
+		}
+		
+    else if ((znak == '*') && (znakDo == '*')) {
+  		result *= peremA;
+		}
+		
+    else if ((znak == '/') && (znakDo == '*')) {
+			result /= peremA;
+		}
+		
+    else if ((znak == '*') && (znakDo == '+')) {
+			result -= peremP;
+			peremP *= peremA;
+			result += peremP;
+			znakDo = '+';
+		}
+		
+    else if ((znak == '*') && (znakDo == '-')) {
+			result += peremP;
+			peremP *= peremA;
+			result -= peremP;
+			znakDo = '-';
+		}
+		
+    else if ((znak == '/') && (znakDo == '+')) {
+			result -= peremP;
+			peremP /= peremA;
+			result += peremP;
+			znakDo = '+';
+		}
+		
+    else if ((znak == '/') && (znakDo == '-')) {
+			result += peremP;
+			peremP /= peremA;
+			result -= peremP;
+			znakDo = '+';
+		}
+		
+    else {
+			cout << "Error";
+			cin.get();
+			return 0;
+		}
+ }	
+  
+  cout << result;
+	cin.get();
+	return 0;
+}
