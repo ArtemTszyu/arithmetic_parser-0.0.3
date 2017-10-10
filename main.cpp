0519e6d036f3baa8835e2a0fb911a7773f8a1e3a
Alexandr Pozhuev <32191131+SashaPozhuev1@users.noreply.github.com>
2017-10-10 23:40:37 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -6,76 +6,76 @@ using namespace std;
 
 int main()
 {	
-	float result, peremA, peremP;
-	char znak, znakDo = '*';
+    float result, peremA, peremP;
+    char znak, znakDo = '*';
 	
-	string stroka;
-	getline(cin, stroka);
-	stroka = "0" + stroka;
+    string stroka;
+    getline(cin, stroka);
+    stroka = "0" + stroka;
 
-	istringstream stream(stroka);
+    istringstream stream(stroka);
 
-	stream >> result;
+    stream >> result;
 	
-	while (stream >> znak) {
-    stream >> peremA;
+    while (stream >> znak) {
+    	stream >> peremA;
 
-		if (znak == '+') {
-			result += peremA;
-			znakDo = '+';
-			peremP = peremA;
-		}
+	if (znak == '+') {
+        result += peremA;
+	znakDo = '+';
+	peremP = peremA;
+        }
 		
-    else if (znak == '-') {
-			result -= peremA;
-			znakDo = '-';
-			peremP = peremA;
-		}
+        else if (znak == '-') {
+	result -= peremA;
+	znakDo = '-';
+	peremP = peremA;
+        }
 		
-    else if ((znak == '*') && (znakDo == '*')) {
-  		result *= peremA;
-		}
+    	else if ((znak == '*') && (znakDo == '*')) {
+       	    result *= peremA;
+	}
 		
-    else if ((znak == '/') && (znakDo == '*')) {
-			result /= peremA;
-		}
+    	else if ((znak == '/') && (znakDo == '*')) {
+	    result /= peremA;
+	}
 		
-    else if ((znak == '*') && (znakDo == '+')) {
-			result -= peremP;
-			peremP *= peremA;
-			result += peremP;
-			znakDo = '+';
-		}
+    	else if ((znak == '*') && (znakDo == '+')) {
+	    result -= peremP;
+	    peremP *= peremA;
+	    result += peremP;
+	    znakDo = '+';
+	}
 		
-    else if ((znak == '*') && (znakDo == '-')) {
-			result += peremP;
-			peremP *= peremA;
-			result -= peremP;
-			znakDo = '-';
-		}
+        else if ((znak == '*') && (znakDo == '-')) {
+	    result += peremP;
+	    peremP *= peremA;
+	    result -= peremP;
+	    znakDo = '-';
+	}
 		
-    else if ((znak == '/') && (znakDo == '+')) {
-			result -= peremP;
-			peremP /= peremA;
-			result += peremP;
-			znakDo = '+';
-		}
+        else if ((znak == '/') && (znakDo == '+')) {
+   	    result -= peremP;
+	    peremP /= peremA;
+	    result += peremP;
+	    znakDo = '+';
+	}
 		
-    else if ((znak == '/') && (znakDo == '-')) {
-			result += peremP;
-			peremP /= peremA;
-			result -= peremP;
-			znakDo = '+';
-		}
+        else if ((znak == '/') && (znakDo == '-')) {
+	    result += peremP;
+	    peremP /= peremA;
+	    result -= peremP;
+	    znakDo = '+';
+	}
 		
-    else {
-			cout << "Error";
-			cin.get();
-			return 0;
-		}
- }	
+        else {
+	    cout << "Error";
+	    cin.get();
+	    return 0;
+	}
+    }	
   
-  cout << result;
-	cin.get();
-	return 0;
+    cout << result;
+    cin.get();
+    return 0;
 }
